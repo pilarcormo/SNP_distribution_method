a9662c10d884406ea515bbad824e43c583ebaa63
pilarcormo <pilarcormo@gmail.com>
2015-04-17 17:02:01 +0100

    R scripts

--- Points.R
+++ Points.R
@@ -1,9 +1,17 @@
-#ratio_positions <- read.csv("~/SNP_distribution_method/arabidopsis_datasets/Genomes_SDM/chr1_A_1/ratio_positions0.csv")
-x <- ratio_positions0$Position
-y <- ratio_positions0$Ratio
+ratio_positions0 <- read.csv("~/SNP_distribution_method/arabidopsis_datasets/OC_chr2_1kb/ratio_positions0_1.csv")
+ratio_positions01 <- read.csv("~/SNP_distribution_method/arabidopsis_datasets/OC_chr2_1kb/ratio_positions0_0.1.csv")
+ratio_positions001 <- read.csv("~/SNP_distribution_method/arabidopsis_datasets/OC_chr2_1kb/ratio_positions0_0.01.csv")
 
-normalized = (y-min(y))/(max(y)-min(y))
+ratio_positions2 <- read.csv("~/SNP_distribution_method/arabidopsis_datasets/OC_chr2_10kb/ratio_positions0_0.01.csv")
 
-g <- ggplot(ratio_positions0, aes(Position, Ratio)) + geom_point(colour="darkblue", shape=21, size = 4) + xlab("Genome position (bp)") + ylab("Normalised ratio") + xlim(0, 30000000)
-g + theme_bw()
+#x <- ratio_positions01$Position
+#y <- ratio_positions01$Ratio
+
+#normalized = (y-min(y))/(max(y)-min(y))
+
+ratio1 <- ggplot(ratio_positions2, aes(Position, Ratio)) + geom_point(colour="darkblue", shape=21, size = 4) + xlab(" ") + ylab("Ratio") + xlim(0, 20000000) + theme_bw()
+#ratio01 <- ggplot(ratio_positions01, aes(Position, Ratio)) + geom_point(colour="darkblue", shape=21, size = 4) + xlab(" ") + ylab("Ratio") + xlim(0, 25000000) + theme_bw() + ggtitle("Factor = 0.1")
+#ratio001 <- ggplot(ratio_positions001, aes(Position, Ratio)) + geom_point(colour="darkblue", shape=21, size = 4) + xlab("Chromosome position (bp)") + ylab("Ratio") + xlim(0, 25000000) + theme_bw() + ggtitle("Factor = 0.01")
+
+#together <- grid.arrange(ratio1, ratio01, ratio001, ncol = 1, main = "1kb contigs")
 
--- density_hist.R
+++ density_hist.R
@@ -5,9 +5,10 @@ ht2 <- read.table("~/SNP_distribution_method/BCF2/BCF2_chromosome3/ht.txt", quot
 hm3 <- read.table("~/SNP_distribution_method/BCF2_parent/chromosome3/hm.txt", quote="\"")
 ht3 <- read.table("~/SNP_distribution_method/BCF2_parent/chromosome3/ht.txt", quote="\"")
 
-data <- c(hm3$V1)
-length <- 20000000
-h<-hist(data, breaks=50, density=50, col="gray", xlab=NULL, xlim =c(0,length), main="Homozygous SNPs on chromosome 3") 
+hm <- read.table("~/SNP_distribution_method/OF/Interesting_2/hm.txt", quote="\"")
+data <- c(hm$V1)
+length <- 25000000
+h<-hist(data, breaks=50, density=50, col="gray", xlab=NULL, xlim =c(10000000,length), main="Homozygous SNPs on chromosome 2") 
 multiplier <- h$counts / h$density
 mydensity <- density(data, adjust = 2)
 mydensity$y <- mydensity$y * multiplier[1]
