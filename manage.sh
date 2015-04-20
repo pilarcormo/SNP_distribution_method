97f788f50c74bc3bfc2a43f7b5693c3e4c42f7f0
pilarcormo <pilarcormo@gmail.com>
2015-04-20 17:39:18 +0100

    clean

---
+++ manage.sh
@@ -0,0 +1,11 @@
+# ruby manage_vcf.rb Aw_sup1-2/Variant_calling 1 WsT sup1
+# ruby manage_vcf.rb Aw_sup1-2/Variant_calling 2 WsT sup1
+# ruby manage_vcf.rb Aw_sup1-2/Variant_calling 3 WsT sup1
+# ruby manage_vcf.rb Aw_sup1-2/Variant_calling 4 WsT sup1
+# ruby manage_vcf.rb Aw_sup1-2/Variant_calling 5 WsT sup1
+
+ruby manage_vcf.rb Aw_sup1-2/Variant_calling 1 WsT sup2
+ruby manage_vcf.rb Aw_sup1-2/Variant_calling 2 WsT sup2
+ruby manage_vcf.rb Aw_sup1-2/Variant_calling 3 WsT sup2
+ruby manage_vcf.rb Aw_sup1-2/Variant_calling 4 WsT sup2
+ruby manage_vcf.rb Aw_sup1-2/Variant_calling 5 WsT sup2
---
+++ ratios.sh
@@ -0,0 +1,18 @@
+
+# ruby SNP_distribution_method_variation.rb OC_chr2_10kb Ratio0_new 0 2
+
+
+ruby SNP_distribution_method_variation.rb sup1_chr1_10kb Ratio0_01 0 0.1
+ruby SNP_distribution_method_variation.rb sup1_chr1_10kb Ratio0_001 0 0.01
+ruby SNP_distribution_method_variation.rb sup1_chr4_10kb Ratio0_01 0 0.1
+ruby SNP_distribution_method_variation.rb sup1_chr4_10kb Ratio0_001 0 0.01
+# ruby SNP_distribution_method_variation.rb OC_chr2_10kb Ratio0_1 0 1
+# ruby SNP_distribution_method_variation.rb OC_chr2_10kb Ratio0_001 0 0.01
+
+
+
+# ruby SNP_distribution_method_variation.rb BCF2_chr3_1kb Ratio0_1 0 1
+# ruby SNP_distribution_method_variation.rb BCF2_chr3_1kb Ratio0_001 0 0.01
+# ruby SNP_distribution_method_variation.rb BCF2_chr3_1kb Ratio0_01 0 0.1
+# # ruby SNP_distribution_method_variation.rb BCF2_chr3_10kb Ratio0_001 0 0.01
+
