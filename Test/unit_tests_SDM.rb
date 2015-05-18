7594ed3ea86fb9624d8a7e2ac2d6fadb068fc135
pilarcormo <pilarcormo@gmail.com>
2015-05-18 17:47:51 +0100

    tests for snpdist and stuff

--- Test/unit_tests_SDM.rb
+++ Test/unit_tests_SDM.rb
@@ -6,7 +6,8 @@ class TestSDM < Test::Unit::TestCase
 		@dic_hm_inv = {11.374979=>["a"], 11.842904=>["b"], 10.120768=>["c"], 5.022447=>["d", "e", "f"]}
 	end
 	def test_sorting
-		perm = SDM::sorting(@dic_hm_inv)
+		perm, mut = SDM::sorting(@dic_hm_inv)
 		assert_equal(["e", "d", "a", "b", "c", "f"], perm)
+		assert_equal(["d", "a", "b", "c"], mut)
 	end
 end 
--- Test/unit_tests_snp_dist.rb
+++ Test/unit_tests_snp_dist.rb
@@ -7,18 +7,22 @@ class TestSNPdist < Test::Unit::TestCase
 
 	def setup
 		@ratios = WriteIt.file_to_floats_array("test/ratios_example.txt")
-		@hyp = SNPdist.hyp_snps(@ratios, 2000)
+		@contig_size = 10
 	end
 
-	def test_hyp_snps
-		assert_kind_of(Array, @hyp)
-		assert_kind_of(Float, @hyp[0])
+	def test_general_positions
+		pos = SNPdist.general_positions(@contig_size, @ratios)
+		assert_equal([10, 20, 30, 40, 50, 60, 70, 70], pos)
 	end
+
+	def test_densities_pos
+		@pos = [10, 20, 30, 40, 50, 60, 70]
+		densities = SNPdist.densities_pos(@ratios, @pos)
+		assert_equal([10, 20, 30, 30, 40, 40, 40, 40, 40, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 60, 60, 70], densities)
+	end 
 
 	def test_ylim
-		ylim_density = SNPdist.get_ylim(@hyp, 2000, 'density')
-		assert_kind_of(Float, ylim_density)
-		ylim_density = SNPdist.get_ylim([1,2,3,4,5], 2000, 'density')
-		assert_equal(0.25, ylim_density)
+		ylim = SNPdist.get_ylim([1,2,3,4,5], 2000, 'density')
+		assert_equal(0.25, ylim)
 	end
 end
