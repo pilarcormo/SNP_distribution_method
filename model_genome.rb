51454d89a99300d0c7c8c117535cb89c448610aa
pilarcormo <pilarcormo@gmail.com>
2015-06-02 15:25:44 +0100

    fix model_genome

--- model_genome.rb
+++ model_genome.rb
@@ -3,48 +3,36 @@ require_relative 'lib/model_genome'
 require_relative 'lib/write_it'
 require 'pp'
 
-
 name = ARGV[0]
-size = 2000
+size = ARGV[1]*1000000
+contig_size = ARGV[2]
+snp = (size/1000)*2
 
-# snp = (size/1000)*2
 
 # make the directory to put data files into
 Dir.mkdir(File.join(Dir.home, "SNP_distribution_method/arabidopsis_datasets/#{name}"))
 
 # Create the lists of homozygous and heterozygous SNPs
-# hm_r = 'hm <- rnorm(#{snp}, #{size}/2, #{snp}*2)' # Causative SNP at/near 10000
-# ht_r = 'ht <- runif(#{snp}, 1, #{size})'   # Genome length of 10000
-# hm, ht = ModelGenome::get_snps(hm_r, ht_r)
+hm_r = 'hm <- rnorm(#{snp}, #{size}/2, #{snp}*2)' # Causative SNP at/near 10000
+ht_r = 'ht <- runif(#{snp}, 1, #{size})'   # Genome length of 10000
+hm, ht = ModelGenome::get_snps(hm_r, ht_r)
 
 
-# puts "There are #{hm.length} homozygous SNPs"
-# puts "There are #{ht.length} heterozygous SNPs"
-# puts "Is there a SNP at the centre of the distribution? -- #{snp_pos.include?(7500000)}"
+puts "There are #{hm.length} homozygous SNPs"
+puts "There are #{ht.length} heterozygous SNPs"
+
 hm, ht = [], []
 
 hm = WriteIt::file_to_ints_array("chromosome1/hm.txt")
 ht = WriteIt::file_to_ints_array("chromosome1/ht.txt")
-# 
-
-# hm = WriteIt::file_to_array("hm.txt")
-# ht = WriteIt::file_to_array("ht.txt")
-
-
-
-# hm = [1000, 1001, 1003, 1004, 1010, 1020]
-# ht = [25, 125, 230, 856, 965, 1100, 1229, 1225, 1436]
-
-# hm.flatten!
-# ht.flatten!
 
 snp_pos = [hm, ht].flatten
 
 
-arabidopsis_c4 = ModelGenome::fasta_to_char_array("TAIR10_chr4.fasta")
+arabidopsis_c1 = ModelGenome::fasta_to_char_array("TAIR10_chr4.fasta")
 puts "Creating the genome..."
-small_genome = arabidopsis_c4[-size..-1] # Genome length of 100 kb
-contig_size = 10 # 100-200 bp
+small_genome = arabidopsis_c1[-size..-1] # Genome length of 100 kb
+
 puts "...and generating the fragments"
 frags = ModelGenome::get_frags(small_genome, contig_size)
 
@@ -61,7 +49,7 @@ vcf = ModelGenome::vcf_array(frags, pos_on_frags, snp_pos_all, hm, ht)
 
 WriteIt::write_data("arabidopsis_datasets/#{name}/frags.fasta", fastaformat_array)
 WriteIt::write_data("arabidopsis_datasets/#{name}/snps.vcf", vcf)
-# WriteIt::write_data("arabidopsis_datasets/#{name}/frags_shuffled.fasta", fastaformat_array_shuf)
-# WriteIt::write_txt("arabidopsis_datasets/#{name}/info", [hm_r, ht_r, "Contig size = #{contig_size}"])
+WriteIt::write_data("arabidopsis_datasets/#{name}/frags_shuffled.fasta", fastaformat_array_shuf)
+WriteIt::write_txt("arabidopsis_datasets/#{name}/info", [hm_r, ht_r, "Contig size = #{contig_size}"])
 WriteIt::write_txt("arabidopsis_datasets/#{name}/hm_snps", hm)
 WriteIt::write_txt("arabidopsis_datasets/#{name}/ht_snps", ht)
--- model_genome_real_hpc.rb
+++ model_genome_real_hpc.rb
@@ -5,37 +5,32 @@ require_relative 'lib/reform_ratio'
 
 name = ARGV[0]
 contig_size = ARGV[1].to_i
-chr = ARGV[2]
+file = ARGV[2]
+chr = ARGV[3]
 
 # make the directory to put data files into
-Dir.mkdir(File.join(Dir.home, "Pilar/SDM/genomes/#{name}"))
+Dir.mkdir(File.join(Dir.home, "SNP_distribution_method/arabidopsis_datasets/#{name}"))
 
 # Create the lists of homozygous and heterozygous SNPs
 fasta_file = "TAIR10_chr#{chr}.fasta"
 genome_length = ReformRatio::genome_length(fasta_file)
 
 #####Real hm and ht SNPs 
-hm = WriteIt::file_to_ints_array("BCF2_chromosome#{chr}/hm.txt")
-ht = WriteIt::file_to_ints_array("BCF2_chromosome#{chr}/ht.txt")
-
-# hm_r = "hm <- rnorm(5000, #{snp}, 1000000)" 
-# ht_r = "ht <- runif(10000, 1, #{genome_length})"
-# hm, ht = ModelGenome::get_snps(hm_r, ht_r)
+hm = WriteIt::file_to_ints_array("#{file}/hm_nocen.txt")
+ht = WriteIt::file_to_ints_array("#{file}/ht_nocen.txt")
 
 
 snp_pos = [hm, ht].flatten
 
-# puts "There are #{hm.length} homozygous SNPs"
-# puts "There are #{ht.length} heterozygous SNPs"
-# puts "Is there a SNP at the centre of the distribution? -- #{snp_pos.include?()}"
+puts "There are #{hm.length} homozygous SNPs"
+puts "There are #{ht.length} heterozygous SNPs"
+
 
 arabidopsis_c = ModelGenome::fasta_to_char_array(fasta_file)
-puts "Fragmenting arabidopsis chromosome 1..."
-# 10-20kb
+puts "Fragmenting arabidopsis chromosome #{chr}..."
 frags = ModelGenome::get_frags(arabidopsis_c1, contig_size)
 puts "Done!"
-puts "Arabidopsis chr length: #{arabidopsis_c.length}/ kb"
-puts "Fragmented seq   length: #{frags.join.length} = close enough? You decide."
+puts "Arabidopsis chr length: #{arabidopsis_c.length} kb"
 puts "You have created #{frags.length} fragments of sizes #{contig_size}-#{contig_size*2}"
 
 # Get the positions of the SNPs on fragments
@@ -45,9 +40,9 @@ fastaformat_array = ModelGenome::fasta_array(frags)
 fastaformat_array_shuf = fastaformat_array.shuffle
 vcf = ModelGenome::vcf_array(frags, pos_on_frags, snp_pos_all, hm, ht)
 
-WriteIt::write_txt("genomes/#{name}/hm_snps", hm)
-WriteIt::write_txt("genomes/#{name}/ht_snps", ht)
-WriteIt::write_data("genomes/#{name}/frags.fasta", fastaformat_array)
-WriteIt::write_data("genomes/#{name}/snps.vcf", vcf)
-WriteIt::write_data("genomes/#{name}/frags_shuffled.fasta", fastaformat_array_shuf)
-# WriteIt::write_txt("genomes/#{name}/info", [hm_r, ht_r, "Contig size = #{contig_size}"])
+WriteIt::write_txt("arabidopsis_datasets/#{name}/hm_snps", hm)
+WriteIt::write_txt("arabidopsis_datasets/#{name}/ht_snps", ht)
+WriteIt::write_data("arabidopsis_datasets/#{name}/frags.fasta", fastaformat_array)
+WriteIt::write_data("arabidopsis_datasets/#{name}/snps.vcf", vcf)
+WriteIt::write_data("arabidopsis_datasets/#{name}/frags_shuffled.fasta", fastaformat_array_shuf)
+
