92280cedb5cfea5c6928aaed411e2482fb7945f9
pilarcormo <pilarcormo@gmail.com>
2015-08-13 13:39:01 +0100

    r

--- R_scripts/Overlap_densities.R
+++ R_scripts/Overlap_densities.R
@@ -1,23 +1,41 @@
 
-hm1 <- read.table("~/SNP_distribution_method/Aw_sup1-2/Variant_calling/sup1_2_1/hm.txt", quote="\"")
-ht1 <- read.table("~/SNP_distribution_method/Aw_sup1-2/Variant_calling/sup1_2_1/ht.txt", quote="\"")
+hm_bcf2 <- read.table("~/SNP_distribution_method/Reads/BCF2/BCF2_chromosome3/interesting_3/hm_nocen.txt", quote="\"")
+ht_bcf2 <- read.table("~/SNP_distribution_method/Reads/BCF2/BCF2_chromosome3/interesting_3/ht_nocen.txt", quote="\"")
+ratio_bcf2 <- read.table("~/SNP_distribution_method/arabidopsis_datasets/No_centromere/10kb_contigs/bcf2_nocen_chr3_10kb/autoratio_1//ratios.txt", quote="\"")
+data_hm <- c(hm_bcf2$V1)      
+data_ht <- c(ht_bcf2$V1)
+ratio <- c(ratio_bcf2$V1)
+length <- 23459830 #chromosome 3 length
+
+hm_ocf2 <- read.table("~/SNP_distribution_method/Reads/OCF2/OCF2_chromosome2/Interesting_2/hm_nocen.txt", quote="\"")
+ht_ocf2 <- read.table("~/SNP_distribution_method/Reads/OCF2/OCF2_chromosome2/Interesting_2/ht_nocen.txt", quote="\"")
+ratio_ocf2 <- read.table("~/SNP_distribution_method/arabidopsis_datasets/No_centromere/10kb_contigs/ocf2_nocen_chr2_10kb/SDM_0/ratios.txt", quote="\"")
+data_hm <- c(hm_ocf2$V1)      
+data_ht <- c(ht_ocf2$V1)
+ratio <- c(ratio_ocf2$V1)
+length <- 19698289 #chromosome 2 length
+
+
 hm5 <- read.table("~/SNP_distribution_method/Reads/Aw_sup1-2/filter2_chromosome4/hm_nocen.txt", quote="\"")
-hm5 <- read.table("~/SNP_distribution_method/Reads/Aw_sup1-2/filter2_chromosome4/ht_nocen.txt", quote="\"")
-hm <- read.table("~/SNP_distribution_method/Reads/Alpina/arabis_bc1f2_1/Interesting_bc1f2/hm2.txt", quote="\"")
-ht <- read.table("~/SNP_distribution_method/Reads/Alpina/arabis_bc1f2_1/hm.txt", quote="\"")
+ht5 <- read.table("~/SNP_distribution_method/Reads/Aw_sup1-2/filter2_chromosome4/ht_nocen.txt", quote="\"")
+ratio <- read.table("~/SNP_distribution_method/arabidopsis_datasets/No_centromere/2-5kb_contigs/sup1_nocen_chr4_5kb/sup1_SDM_0/ratios.txt", quote="\"")
+
+options(scipen = 10)
+
 #ratio <- read.table("~/SNP_distribution_method/Reads/m_mutants/C_chromosome5/interesting_5/ratios_BC.txt", quote="\"")
-ratio <- read.table("~/SNP_distribution_method/arabidopsis_datasets/No_centromere/sup1_nocen_chr4_10kb/SDM_0/ratios.txt", quote="\"")
+
 ratio2 <- read.table("~/SNP_distribution_method/arabidopsis_datasets/No_centromere/old/sup1_nocen_chr4_100kb/SDM_off/ratios.txt", quote="\"")
-data_hm <- c(hm4$V1)      
-data_ht <- c(ht4$V1)
+data_hm <- c(hm5$V1)      
+data_ht <- c(ht5$V1)
 ratio <- c(ratio$V1)
 ratio2 <- c(ratio$V1)
 length <- 18585056
 
 
 ########PLOT DENSITIES######## 
-d1 <-density(data_hm, adjust = 2, kernel = c("gaussian"))
-d2 <- density(data_ht, adjust = 2, kernel = c("gaussian"))
+
+d1 <-density(data_hm, adjust = 1, kernel = c("gaussian"))
+d2 <- density(data_ht, adjust = 1, kernel = c("gaussian"))
 d3 <- density(ratio, adjust = 2, kernel = c("gaussian"))
 p1 <- plot(range(d1$x, d2$x, d3$x), range(d1$y, d2$y, d3$y), type = "n", main = "Chromosome 4", xlim =c(0,length), xlab = " ", ylab = " ")
 lines(d1, col = 'magenta') ##Homozygous 
--- R_scripts/SNP_filtering.R
+++ R_scripts/SNP_filtering.R
@@ -13,28 +13,29 @@ density_sum_out$cross <- factor(density_sum_out$cross, labels = c( "OCF2", "sup1
 #density_sum_out$type <- factor(density_sum_out$type, levels = c("Raw VCF","Parental filtering","Centromere removal", "Candidate SNPs"))
 #density_sum_back$type <- factor(density_sum_back$type, levels = c("Raw VCF","Parental filtering","Centromere removal", "Candidate SNPs"))
 
+
 Palette <- brewer.pal(2,"Set1")
 Palette2<-brewer.pal(3,"Set2")
 
 g <- ggplot(density_sum_back, aes(x = type, y = density, shape = cross, colour = cross)) + geom_point(size=3) + geom_line(data = subset(density_sum_back, cross %in% c("BCF2", "mob1", "mob2")), aes(group = cross)) + scale_x_discrete(limits=c("Raw VCF","Parental filtering","Centromere removal"))  +labs(x = "",  y = "Absolute SNP density")  
-g <- g +theme_bw()
-g <- g + theme_minimal(base_size = 15) +  scale_color_manual(values=Palette2, name  ="Back-cross") + scale_shape_discrete(name  ="Back-cross")+  theme(legend.position="none")
+g <- g +theme_bw(base_size = 20)
+g <- g + scale_color_manual(values=Palette2, name  ="Back-cross") + scale_shape_discrete(name  ="Back-cross")+  theme(legend.position="none")
 
 
 t <- ggplot(density_sum_back, aes(x = type, y = density, shape = cross, colour = cross)) + geom_point(size=3) + geom_line(data = subset(density_sum_back, cross %in% c("BCF2", "mob1", "mob2")), aes(group = cross)) + scale_x_discrete(limits=c("Centromere removal", "Candidate SNPs"))+ ylim(0, 100)  + labs(x = "",  y = "Absolute SNP density") 
-t <- t + theme_bw() 
-t <- t + theme_minimal(base_size = 15) +  scale_color_manual(values=Palette2, name  ="Back-cross") + scale_shape_discrete(name  ="Back-cross") 
+t <- t + theme_bw(base_size = 20) 
+t <- t +  scale_color_manual(values=Palette2, name  ="Back-cross") + scale_shape_discrete(name  ="Back-cross") 
 
 
 
 s <- ggplot(density_sum_out, aes(x = type, y = density, shape = cross, colour = cross)) + geom_point(size=3) + geom_line(data = subset(density_sum_out, cross %in% c("OCF2", "sup1")), aes(group = cross)) + scale_x_discrete(limits=c("Centromere removal", "Candidate SNPs"))+ ylim(0, 10000) + labs(x = " ",  y = "Absolute SNP density")
-s <- s +theme_bw() 
-s <- s+ theme_minimal(base_size = 15) +  scale_color_manual(values=Palette, name  ="Out-cross")+ scale_shape_discrete(name  ="Out-cross")
+s <- s +theme_bw(base_size = 20) 
+s <- s +  scale_color_manual(values=Palette, name  ="Out-cross")+ scale_shape_discrete(name  ="Out-cross")
 
 
 h <- ggplot(density_sum_out, aes(x = type, y = density, shape = cross, colour = cross)) + geom_point(size=3) + geom_line(data = subset(density_sum_out, cross %in% c("OCF2", "sup1")), aes(group = cross)) + scale_x_discrete(limits=c("Raw VCF","Parental filtering","Centromere removal"))+ labs(x = " ",  y = "Absolute SNP density")  
-h <- h + theme_bw()
-h <- h+ theme_minimal(base_size = 15) + scale_color_manual(values=Palette, name  ="Out-cross")+ scale_shape_discrete(name  ="Out-cross") + theme(legend.position="none")
+h <- h + theme_bw(base_size = 20) 
+h <- h+ scale_color_manual(values=Palette, name  ="Out-cross")+ scale_shape_discrete(name  ="Out-cross") + theme(legend.position="none")
 
 
 ts <- grid.arrange(t, s,  ncol=2, as.table =TRUE)
