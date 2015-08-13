b9d5e104112527243cda53e89fe02bf0f387063f
pilarcormo <pilarcormo@gmail.com>
2015-08-13 13:37:33 +0100

    update model genome script for chr1

--- Small_genomes/chr1_model_genome.rb
+++ Small_genomes/chr1_model_genome.rb
@@ -1,45 +1,44 @@
 #encoding: utf-8
-require_relative '../lib/model_genome'
+require_relative 'lib/model_genome'
 require_relative '../lib/write_it'
+require_relative  'lib/reform_ratio'
 require 'pp'
 
 name = ARGV[0]
 contig_size = ARGV[1].to_i
-snp = (size/1000)*2
 
 fasta_file = "TAIR10_chr1.fasta"
+
 genome_length = ReformRatio::genome_length(fasta_file)
 
+genome_length.to_i
+
+snp = genome_length/3000
 snp_2 = snp*2
-size_2 = size/2
+size_2 = genome_length/2
 # make the directory to put data files into
 Dir.mkdir("arabidopsis_datasets/#{name}")
 
-# Create the lists of homozygous and heterozygous SNPs
 hm_r = "hm <- rnorm(#{snp}, #{size_2}, #{snp_2})" # Causative SNP at/near 10000
-ht_r = "ht <- runif(#{snp}, 1, #{size})"   # Genome length of 10000
+ht_r = "ht <- runif(#{snp}, 1, #{genome_length})"   # Genome length of 10000
 hm, ht = ModelGenome::get_snps(hm_r, ht_r)
 
+arabidopsis_c1 = ModelGenome::fasta_to_char_array(fasta_file)
 
 puts "There are #{hm.length} homozygous SNPs"
 puts "There are #{ht.length} heterozygous SNPs"
 
 snp_pos = [hm, ht].flatten
 
-
-arabidopsis_c1 = ModelGenome::fasta_to_char_array(fasta_file)
-genome_length = ReformRatio::genome_length(arabidopsis_c1)
-
 puts "...and generating the fragments"
-frags = ModelGenome::get_frags(genome_length, contig_size)
 
-puts "Small genome length: #{genome_length.length} bases"
+frags = ModelGenome::get_frags(arabidopsis_c1, contig_size)
+
+puts "Small genome length: #{genome_length} bases"
 puts "You have created #{frags.length} fragments of sizes #{contig_size}-#{contig_size*2}"
-
 
 # Get the positions of the SNPs on fragments
 pos_on_frags, snp_pos_all = ModelGenome::pos_each_frag(snp_pos, frags)
-
 
 fastaformat_array = ModelGenome::fasta_array(frags)
 fastaformat_array_shuf = fastaformat_array.shuffle
